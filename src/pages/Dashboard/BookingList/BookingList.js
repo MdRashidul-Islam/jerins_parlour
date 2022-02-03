be6a05bb185a01a42edf003cba587d5755a58dfe
rashidulronye <rashidulislam.official1@gamil.com>
2022-02-03 15:00:01 +0600

    upate

--- src/pages/Dashboard/BookingList/BookingList.js
+++ src/pages/Dashboard/BookingList/BookingList.js
@@ -1,11 +1,105 @@
 import React from "react";
+import styled from "styled-components";
+import pic from "../../../assets/Icon/Group 1373.png";
 
 const BookingList = () => {
   return (
-    <div>
-      <h1>Booking List</h1>
-    </div>
+    <BookingListStyled>
+      <div className="card_section">
+        <div className="card">
+          <div className="card_header">
+            <img src={pic} alt="" />
+            <button>Pending</button>
+          </div>
+          <div className="card_info">
+            <h3>Anti Age Face Treatment</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+              consequuntur, distinctio nostrum repellat porro repellendus
+              dignissimos libero sapiente. Possimus quae.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card_header">
+            <img src={pic} alt="" />
+            <button>Pending</button>
+          </div>
+          <div className="card_info">
+            <h3>Anti Age Face Treatment</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+              consequuntur, distinctio nostrum repellat porro repellendus
+              dignissimos libero sapiente. Possimus quae.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card_header">
+            <img src={pic} alt="" />
+            <button>Pending</button>
+          </div>
+          <div className="card_info">
+            <h3>Anti Age Face Treatment</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+              consequuntur, distinctio nostrum repellat porro repellendus
+              dignissimos libero sapiente. Possimus quae.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card_header">
+            <img src={pic} alt="" />
+            <button>Pending</button>
+          </div>
+          <div className="card_info">
+            <h3>Anti Age Face Treatment</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+              consequuntur, distinctio nostrum repellat porro repellendus
+              dignissimos libero sapiente. Possimus quae.
+            </p>
+          </div>
+        </div>
+      </div>
+    </BookingListStyled>
   );
 };
+
+const BookingListStyled = styled.div`
+  .card_section {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-content: space-around;
+    grid-row-gap: 10px;
+
+    .card {
+      width: 300px;
+      height: 100%;
+      border: none;
+      padding: 10px;
+      background-color: white;
+      border-radius: 5px;
+
+      .card_header {
+        display: flex;
+        align-items: center;
+        img {
+          width: 30%;
+          margin-right: 100px;
+          padding: 10px;
+        }
+        button {
+          padding: 10px 20px;
+          border: none;
+          background-color: #ffecf2;
+          border-radius: 3px;
+          color: #f86a99;
+        }
+      }
+    }
+  }
+`;
 
 export default BookingList;
--- src/pages/Dashboard/Review/Review.js
+++ src/pages/Dashboard/Review/Review.js
@@ -1,11 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "../../Shared/Button/Button";
 
 const Review = () => {
   return (
-    <div>
-      <h1>Review section</h1>
-    </div>
+    <ReviewStyled>
+      <div className="review_section">
+        <form className="form">
+          <input type="text" placeholder="Your Name" />
+          <input type="text" placeholder="Your Email" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Description"
+          ></textarea>
+          <div className="button">
+            <Button title={"Submit"}></Button>
+          </div>
+        </form>
+      </div>
+    </ReviewStyled>
   );
 };
+
+const ReviewStyled = styled.div`
+  .review_section {
+    .form {
+      width: 350px;
+
+      input {
+        display: block;
+        width: 100%;
+        height: 40px;
+        margin-bottom: 10px;
+        border: none;
+        border-radius: 3px;
+        padding: 3px;
+      }
+      textarea {
+        width: 100%;
+        border: none;
+        border-radius: 3px;
+        padding: 3px;
+      }
+    }
+    .button {
+      button {
+        padding: 6px 20px;
+      }
+    }
+  }
+`;
 
 export default Review;
